c6a790dc413c5cdb79a9e02b0f011a3b4b26b9b6
BatoBalint <balint.bato@gmail.com>
2021-10-16 13:24:05 +0200

    kesz az init es a default settings, alakulnak az eventlistenerek

--- app/src/main/java/com/example/coinflip/MainActivity.kt
+++ app/src/main/java/com/example/coinflip/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.coinflip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var coin : ImageView
+    lateinit var btnFej : Button
+    lateinit var btnIras : Button
+    lateinit var txtDobas : TextView
+    lateinit var txtWin : TextView
+    lateinit var txtLose : TextView
+    var dobasok = 0
+    var win = 0
+    var lose = 0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Init()
+        DefaultSetting()
+    }
+
+    fun Init() {
+        coin = findViewById(R.id.coin_img)
+        btnFej = findViewById(R.id.btn_fej)
+        btnIras = findViewById(R.id.btn_iras)
+        txtDobas = findViewById(R.id.txt_dobas)
+        txtWin = findViewById(R.id.txt_win)
+        txtLose = findViewById(R.id.txt_lose)
+
+        btnFej.setOnClickListener {
+            Toast("Test");
+        }
+    }
+
+    fun DefaultSetting() {
+        dobasok = 0
+        win = 0
+        lose = 0
     }
 }
